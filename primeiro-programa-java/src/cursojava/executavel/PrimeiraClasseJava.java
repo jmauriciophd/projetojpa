2e10733a76caa1dc100b28c6cf2c3d4c3d1eb9fe
Mauricio Gomes <jmauriciophd@gmail.com>
2022-07-06 13:50:57 -0300

    Criação do projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,11 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Meu primeiro programa em Java\n");
+			System.out.println("Meu primeiro programa em Java");
+	}
+
+}
